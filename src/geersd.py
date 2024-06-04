a8b937dac74dca57f3a3ce978f97f6d7bca2ca49
Ryan Hamilton <ryangilberthamilton@gmail.com>
2024-06-04 10:17:14 -0400

    add sentinel2 class and examples

--- src/geersd.py
+++ src/geersd.py
@@ -34,3 +34,37 @@ class Sentinel1(ee.ImageCollection):
         edge = image.lt(-30.0)
         masked_image = image.mask().And(edge.Not())
         return image.updateMask(masked_image)
+
+
+class Sentinel2(ee.ImageCollection):
+    @classmethod
+    def surface_reflectance(cls):
+        return cls("COPERNICUS/S2_SR_HARMONIZED")
+    
+    @classmethod
+    def top_of_atmosphere(cls):
+        return cls("COPERNICUS/S2_HARMONIZED")
+
+    def __init__(self, args: Any):
+        super().__init__(args)
+
+    def filterCloud(self, percent: float):
+        return self.filter(ee.Filter.lte('CLOUDY_PIXEL_PERCENTAGE', percent))
+
+    def applyCloudMask(self):
+        return self.map(self.cloud_mask)
+    
+    @staticmethod
+    def cloud_mask(image: ee.Image):
+        qa = image.select('QA60')
+        # Bits 10 and 11 are clouds and cirrus, respectively.
+        cloud_bit_mask = 1 << 10
+        cirrus_bit_mask = 1 << 11
+        # Both flags should be set to zero, indicating clear conditions.
+        mask = (
+            qa.bitwiseAnd(cloud_bit_mask)
+            .eq(0)
+            .And(qa.bitwiseAnd(cirrus_bit_mask).eq(0))
+        )
+
+        return image.updateMask(mask)
